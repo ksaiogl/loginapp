146240706be3e815e26d56f3210134e1a751e45c
ksaiogl <k.sairohith94@gmail.com>
2017-01-17 03:12:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://54.213.4.128:27017/localDB');
+mongoose.connect('mongodb://54.213.4.128:27017/loginapp');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
